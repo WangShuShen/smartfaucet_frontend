04693413ebc4f5048ceaefbdb5e19736742f2db2
woody <woody38921218@gmail.com>
2024-02-19 06:59:31 +0000

    feat: add projectlistblock label select color

--- app/project_setting/content_management_segment/projectlistblock_component/projectlistblock_component.tsx
+++ app/project_setting/content_management_segment/projectlistblock_component/projectlistblock_component.tsx
@@ -8,6 +8,11 @@ export default function ProjectListBlockComponent() {
   const dispatch = useDispatch();
   const [projects, setProjects] = useState([]);
   const [selectedId, setSelectedId] = useState(null);
+  const [hasUpdated, setHasUpdated] = useState({
+    company: false,
+    building: false,
+    floor: false,
+  });
   const emptyRows = Math.max(5 - projects.length, 0);
   const emptyRowsArray = Array(emptyRows).fill(null);
 
@@ -21,7 +26,8 @@ export default function ProjectListBlockComponent() {
       console.log("未找到選中的項目");
     }
   };
-  const handleAddFloor = () => {
+  const handleAddFloor = (id) => {
+    setSelectedId(id);
     dispatch(setNotification("新增Floor Management"));
   };
   const companyValue = useSelector(
@@ -48,20 +54,54 @@ export default function ProjectListBlockComponent() {
           location: "",
         },
       ]);
-
+      setHasUpdated((prev) => ({
+        ...prev,
+        company: true,
+        building: false,
+        floor: false,
+      }));
       setSelectedId(newId);
     }
   }, [companyValue]);
 
   useEffect(() => {
     if (selectedId && buildingValue) {
-      setProjects((prevProjects) =>
-        prevProjects.map((project) =>
+      const selectedProjectIndex = projects.findIndex(
+        (project) => project.id === selectedId
+      );
+      const selectedProject = projects[selectedProjectIndex];
+
+      // 檢查是否已有建築物值
+      if (selectedProject && selectedProject.building) {
+        // 如果已有初始值，創建新的 row
+        const newProject = {
+          id: projects.length + 1,
+          company: selectedProject.company, // 從上一個 row 複製 companyValue
+          building: buildingValue, // 設置新的 buildingValue
+          floor: "",
+          hub: "",
+          location: "",
+        };
+
+        // 添加新的 row 到列表
+        setProjects((prevProjects) => [...prevProjects, newProject]);
+        setSelectedId(newProject.id); // 選中新添加的 row
+      } else {
+        // 如果沒有初始值，更新當前選中的 row
+        const updatedProjects = projects.map((project) =>
           project.id === selectedId
             ? { ...project, building: buildingValue }
             : project
-        )
-      );
+        );
+
+        setProjects(updatedProjects);
+        setHasUpdated((prev) => ({
+          ...prev,
+          company: false,
+          building: true,
+          floor: false,
+        }));
+      }
     }
   }, [buildingValue]);
   useEffect(() => {
@@ -73,6 +113,12 @@ export default function ProjectListBlockComponent() {
             : project
         )
       );
+      setHasUpdated((prev) => ({
+        ...prev,
+        company: false,
+        building: false,
+        floor: true,
+      }));
     }
   }, [FloorValue]);
   return (
@@ -82,16 +128,34 @@ export default function ProjectListBlockComponent() {
           <table className="min-w-full leading-normal">
             <thead>
               <tr>
-                <th className="sticky top-0 z-10 px-5 py-3 bg-[#EFEFEF] text-left text-xs text-[#5F6162] uppercase tracking-wider">
+                <th className="sticky top-0 z-10 px-5 py-3 bg-[#EFEFEF] text-left text-xs text-[#5F6162] uppercase tracking-wider ${hasUpdated.company ? 'bg-[#007BFF]' : 'bg-[#EFEFEF]'} text-[#5F6162]`}">
                   選取
                 </th>
-                <th className="sticky top-0 z-10 px-5 py-3 bg-[#A9CFD9] text-left text-xs text-[#5F6162] uppercase tracking-wider">
+                <th
+                  className={`sticky top-0 z-10 px-5 py-3 text-left text-xs uppercase tracking-wider ${
+                    hasUpdated.company
+                      ? "bg-[#1498b5] text-[#FFFFFF]"
+                      : "bg-[#A9CFD9] text-[#5F6162]"
+                  } `}
+                >
                   專案/公司
                 </th>
-                <th className="sticky top-0 z-10 px-5 py-3 bg-[#A9CFD9] text-left text-xs text-[#5F6162] uppercase tracking-wider">
+                <th
+                  className={`sticky top-0 z-10 px-5 py-3 text-left text-xs uppercase tracking-wider ${
+                    hasUpdated.building
+                      ? "bg-[#1498b5] text-[#FFFFFF]"
+                      : "bg-[#A9CFD9] text-[#5F6162]"
+                  } `}
+                >
                   任務/建築物
                 </th>
-                <th className="sticky top-0 z-10 px-5 py-3 bg-[#A9CFD9] text-left text-xs text-[#5F6162] uppercase tracking-wider">
+                <th
+                  className={`sticky top-0 z-10 px-5 py-3 text-left text-xs uppercase tracking-wider ${
+                    hasUpdated.floor
+                      ? "bg-[#1498b5] text-[#FFFFFF]"
+                      : "bg-[#A9CFD9] text-[#5F6162]"
+                  }`}
+                >
                   樓層
                 </th>
                 <th className="sticky top-0 z-10 px-5 py-3 bg-[#A9CFD9] text-left text-xs text-[#5F6162] uppercase tracking-wider">
@@ -145,7 +209,7 @@ export default function ProjectListBlockComponent() {
                     {project.floor}
                     {project.building && !project.floor && (
                       <button
-                        onClick={() => handleAddFloor()}
+                        onClick={() => handleAddFloor(project.id)}
                         className="absolute left-6 top-1/2 transform -translate-y-1/2"
                         style={{ outline: "none" }}
                       >
