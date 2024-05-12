13a0c9b0f282ce286d3f95ca4e85a910c7b5b6b1
woody <woody38921218@gmail.com>
2024-05-12 14:18:14 +0000

    feat: revise deploy file

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,13 @@ FROM node:21.6.2
 
 WORKDIR /app
 
+COPY . .
+
 COPY package*.json ./
 
 RUN npm install
+
+RUN npm run build
 
 COPY . .
 
--- run_env.sh
+++ run_env.sh
@@ -3,19 +3,19 @@
 # 設定容器名稱
 CONTAINER_NAME="smart_faucet_frontend"
 
-docker stop ${CONTAINER_NAME}
-docker rm ${CONTAINER_NAME}
+sudo docker stop ${CONTAINER_NAME}
+sudo docker rm ${CONTAINER_NAME}
 
 # 執行npm build
 echo "正在建構前端應用..."
-npm run build
+# npm run build
 
 # 建立 Docker 鏡像
 echo "正在建立 Docker 鏡像..."
-docker build -t ${CONTAINER_NAME} .
+sudo docker build -t ${CONTAINER_NAME} .
 
 # 運行容器
 echo "正在啟動容器..."
-docker run -d -p 39684:3000 --name ${CONTAINER_NAME} ${CONTAINER_NAME}
+sudo docker run -d -p 39684:3000 --name ${CONTAINER_NAME} ${CONTAINER_NAME}
 
 echo "部署完成！"
